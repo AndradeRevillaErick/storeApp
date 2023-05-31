17c0e1790efd27e0a452eb4c37ae296cef53d616
Erick Andrade <erick_andradhe@hotmail.com>
2023-05-31 11:06:20 -0600

    config(redux): initial configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { Test } from "./components/Test";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <Provider store={store}>
+      <Test />
+    </Provider>
   </React.StrictMode>
-)
+);
