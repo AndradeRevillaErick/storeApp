352f68e0657e69f7c5c08baa493b748a042b1be6
Erick Andrade <erick_andradhe@hotmail.com>
2023-08-11 13:53:44 -0600

    test(pokeapi): add a test usign axios

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { Test } from "./components/Test";
+// import { Test } from "./components/Test";
+import { PokemonApp } from "./PokemonApp";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Test />
+      {/* <Test /> */}
+      <PokemonApp />
     </Provider>
   </React.StrictMode>
 );
